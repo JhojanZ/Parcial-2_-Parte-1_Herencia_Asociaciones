f16a08d7f0304576576bb9a3c5ff2546c5fe5666
Dayana Rios <d.rios1@utp.edu.co>
2024-10-31 19:04:38 -0500

    Adición y arreglos de funciones

--- ui/ui.py
+++ ui/ui.py
@@ -1,89 +1,79 @@
 from modelo.cliente import Cliente
 from modelo.factura import Factura
+from CRUD.crud import ClienteCrud, ProductoControlCrud
 
 class UI:
     def __init__(self):
         # Si hay tiempo, cambiar la lista de clientes por un diccionario
-        self.clientes = []
+        self.clientes = {}
         self.productos = []
 
     def mostrar_menu(self):
-        # falta arreglar los numeros 
-        # (eliminar si se arreglo)
+
         print("1. Agregar clientes")
-        print("2. |Agregar productos")
+        print("2. Agregar productos")
         print("3. Eliminar clientes")
         print("4. Modificar clientes")
-        print("5. |Modificar productos")
-        print("6. Mostrar cantidad de algún producto")
-        print("7. Mostrar lista de clientes")
+        print("5. Modificar productos")
+        print("6. Mostrar lista de clientes")
+        print("7. Mostrar la factura de algún cliente")
         print("8. Mostrar la factura de algún cliente")
-        print("9. Mostrar la factura de algún cliente")
-        print("10. Buscar por cedula")
+        print("9. Buscar por cedula")
         
-        print("11. Salir")
+        print("10. Salir")
         
     def buscar_por_cedula(self):
         dni = input("Ingrese el DNI del cliente: ")
-        for cliente in self.clientes:
-            if cliente.dni == dni:
-                return cliente
-        
-        print(f"No se encontró un cliente con el DNI {dni}.")
-        return None
+        cliente = self.cliente_crud.buscar_por_cedula(dni)  
+        return cliente
 
     def agregar_cliente(self):
         nombre = input("Ingrese el nombre del cliente: ")
         dni = input("Ingrese el DNI del cliente: ")
-        
-        nuevo_cliente = Cliente(nombre, dni)
-        self.clientes.append(nuevo_cliente)
-        
-        print(f"Cliente {nombre} agregado exitosamente.")
+        self.cliente_crud.crear_cliente(nombre, dni)
 
     def agregar_producto(self):
-        # implementar metodo
-        pass
+        registro_ica = input("Ingrese el registro ICA del producto: ")
+        nombre_producto = input("Ingrese el nombre del producto: ")
+        frecuencia_aplicacion = input("Ingrese la frecuencia de aplicación: ")
+        valor = float(input("Ingrese el valor del producto: "))
+        cantidad = int(input("Ingrese la cantidad disponible: "))
+        self.producto_crud.crear_producto(registro_ica, nombre_producto, frecuencia_aplicacion, valor, cantidad)
 
     def eliminar_cliente(self):
         cliente_encontrado = self.buscar_por_cedula()
 
         if cliente_encontrado:
-            self.clientes.remove(cliente_encontrado)
+            self.cliente_crud.eliminar_cliente(cliente_encontrado.dni)
 
     def modificar_cliente(self):
         cliente_encontrado = self.buscar_por_cedula()
         if cliente_encontrado:
             nuevo_nombre = input("Ingrese el nombre")
-        pass
+            self.cliente_crud.actualizar_cliente(cliente_encontrado.dni, nuevo_nombre)
 
     def modificar_producto(self):
-        # Implementar lógica para modificar producto
-        pass
-
-    def mostrar_cantidad_producto(self):
-        dni = input("Ingrese el DNI del cliente: ")
+        registro_ica = input("Ingrese el registro ICA del producto a modificar: ")
+        nuevo_nombre = input("Ingrese el nuevo nombre del producto: ")
+        nuevo_valor = float(input("Ingrese el nuevo valor del producto: "))
+        nueva_cantidad = int(input("Ingrese la nueva cantidad disponible: "))
+        self.producto_crud.actualizar_producto(registro_ica, nuevo_nombre, nuevo_valor, nueva_cantidad)
 
 
     def mostrar_lista_clientes(self):
-        if not self.clientes:
+        clientes = self.cliente_crud.leer_clientes()
+        if not clientes:
             print("No hay clientes registrados.")
         else:
-            for cliente in self.clientes:
-                print(f"Nombre: {cliente.nombre}, DNI: {cliente.dni}, Factura: {cliente.telefono}")
+            for cliente in clientes:
+                print(f"Nombre: {cliente.nombre}, DNI: {cliente.dni}")
 
-    def mostrar_factura_cliente(self):  
-        cliente_encontrado = self.buscar_por_cedula()
-        
-        if cliente_encontrado:
-            cliente_encontrado.mostrar_facturas()
 
     def crear_factura(self):
         cliente_encontrado = self.buscar_por_cedula()
 
         if cliente_encontrado:
             nueva_factura = Factura()
-            nueva_factura.crear_factura()
             cliente_encontrado.agregar_factura(nueva_factura)
             pass
 
@@ -103,16 +93,14 @@ class UI:
             elif opcion == '5':
                 self.modificar_producto()
             elif opcion == '6':
-                self.mostrar_cantidad_producto()
-            elif opcion == '7':
                 self.mostrar_lista_clientes()
-            elif opcion == '8':
+            elif opcion == '7':
                 self.mostrar_factura_cliente()
-            elif opcion == '9':
+            elif opcion == '8':
                 self.crear_factura()
-            elif opcion == '10':
+            elif opcion == '9':
                 self.buscar_por_cedula()
-            elif opcion == '11':
+            elif opcion == '10':
                 print("Saliendo...")
                 break
             else:
